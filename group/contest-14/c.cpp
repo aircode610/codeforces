0155c470448b2553642dff6ba89fa39cb5f4ef9e
aircode610 <aircode610@gmail.com>
2021-01-12 13:47:37 +0330

    solved a math problem ;)

--- group/contest-14/c.cpp
+++ group/contest-14/c.cpp
@@ -1,42 +1,31 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h>
 #define int long long
-const int M=2e5+10;
-const int MOD=998244353;
-//
+
+using namespace std;
+
+const int M = 2e5+10, MOD = 998244353;
+
+int p[M];
+
 #define _ %MOD
 #define __ %=MOD
-int fac[M],ifac[M];
-int pw(int a,int b)
-{
-  if(b==0LL)return 1LL;
-  int p=pw(a,b/2LL);
-  if(b%2LL==0LL)
-    return p*p _;
-  return p*p _ * a _;
-}
-int inv(int a)
-{
-  return pw(a,MOD-2);
-}
-int comb(int a,int b)
-{
-  if (b > a) return 0;
-  return fac[a]*ifac[b] _ *ifac[a-b] _;//a!/(b!(a-b)!)
-}
-void prec()
-{
-  fac[0]=ifac[0]=1LL;
-  for(int i=1;i<M;i++)
-    {
-      fac[i]=fac[i-1]*i _ ;
-      ifac[i]=inv(fac[i]);
-    }
-}
 
 int32_t main() {
-
-  
-  
+  int n; cin >> n;
+  int ans = 0;
+  p[0] = 1;
+  for (int i = 1; i <= n; i++){
+    p[i] = (10 * p[i-1]) _;
+  }
+  for (int i = 1; i <= n; i++){
+    if (n == i){
+      ans = 10;
+      cout << ans << endl;
+      continue;
+    }
+    ans = (2 * 10 * 9 * p[n-i-1]) + ((n-i-1) * 10 * 9 * 9 * p[n-i-2]);
+    ans __;
+    cout << ans << ' ';
+  }
   return 0;
 }
